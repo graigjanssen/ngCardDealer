8805510a002acce961047c756ee77e3a00767027
Graig Janssen <graig.janssen@gmail.com>
2019-01-26 14:39:27 -0500

    Logic for generating deck based on filter params

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,48 @@ import { Component } from '@angular/core';
 export class AppComponent {
   cardValues = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
   handFilters = {
-    suits: [],
-    size: 1,
-    min: '',
-    max: ''
+    suits: {
+      spade: true,
+      club: true,
+      diamond: true,
+      heart: true
+    },
+    size: 5,
+    min: '2',
+    max: 'A'
   }
+  deck: Card[];
+  hand: Card[];
+
+  onSubmit() {
+    this.deck = this.generateDeck();
+    console.log('heres da deck beetch: ', this.deck);
+    this.hand = this.deal(this.deck);
+  }
+
+  generateDeck(): Card[] {
+    const deck = [];
+
+    const suits = Object.keys(this.handFilters.suits).filter(suit => this.handFilters.suits[suit]);
+    
+    const start = this.cardValues.indexOf(this.handFilters.min);
+    const end = this.cardValues.indexOf(this.handFilters.max) + 1;
+    const values = this.cardValues.slice(start, end);
+
+    suits.forEach(suit => {
+      values.forEach(value => {
+        deck.push({value, suit});
+      });
+    });
+    return deck;
+  }
+
+  deal(deck): Card[] {
+    return [];
+  }
+}
+
+interface Card {
+  value: string;
+  suit: string;
 }
