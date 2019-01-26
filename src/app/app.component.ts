e1439058e3b2f9f2a6cf6d230cc1332d03620b92
Graig Janssen <graig.janssen@gmail.com>
2019-01-26 16:12:35 -0500

    Finish card dealing logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,38 +14,66 @@ export class AppComponent {
       diamond: true,
       heart: true
     },
-    size: 5,
+    size: 1,
     min: '2',
     max: 'A'
   }
   deck: Card[];
   hand: Card[];
+  maxHandSize = 52;
 
   onSubmit() {
     this.deck = this.generateDeck();
-    console.log('heres da deck beetch: ', this.deck);
     this.hand = this.deal(this.deck);
+  }
+
+  updateMaxHandSize() {
+    const deckProperties = this.getDeckProperties();
+
+    this.maxHandSize = deckProperties.suits.length * deckProperties.values.length;
   }
 
   generateDeck(): Card[] {
     const deck = [];
 
+    const deckProperties = this.getDeckProperties();
+
+    deckProperties.suits.forEach(suit => {
+      deckProperties.values.forEach(value => {
+        deck.push({value, suit});
+      });
+    });
+
+    return deck;
+  }
+
+  deal(deck: Card[]): Card[] {
+    const indices = [];
+    const hand = [];
+    while (indices.length < this.handFilters.size) {
+      const randomIndex = this.getRandomIndex();
+      if (indices.indexOf(randomIndex) === -1) {
+        indices.push(randomIndex);
+      }
+    }
+
+    indices.forEach(index => { hand.push(deck[index]); });
+
+    return hand;
+  }
+
+  getDeckProperties() {
     const suits = Object.keys(this.handFilters.suits).filter(suit => this.handFilters.suits[suit]);
     
     const start = this.cardValues.indexOf(this.handFilters.min);
     const end = this.cardValues.indexOf(this.handFilters.max) + 1;
     const values = this.cardValues.slice(start, end);
 
-    suits.forEach(suit => {
-      values.forEach(value => {
-        deck.push({value, suit});
-      });
-    });
-    return deck;
+    return {suits, values};
   }
 
-  deal(deck): Card[] {
-    return [];
+  getRandomIndex() {
+    return Math.floor(Math.random() * this.deck.length);
   }
 }
 
